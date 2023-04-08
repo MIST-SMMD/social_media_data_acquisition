bef018345cd28d60b92098ca1a566b03bfbabbd4
hz157 <horizon_157@outlook.com>
2023-04-08 19:51:04 +0800

    Modify file download location

--- Network/Files.py
+++ Network/Files.py
@@ -16,7 +16,7 @@ from Utils.logutils import LogUtils
 logutils = LogUtils()
 
 
-def DownloadWeiboImage(pid):
+def DownloadWeiboImage(mid, pid):
     """
     下载微博图片
     :param pid: 图片唯一标识
@@ -25,15 +25,20 @@ def DownloadWeiboImage(pid):
     try:
         IMAGE_URL = f"{config.Sina_OrgImage_Url}{pid}.jpg"
         r = requests.get(IMAGE_URL, headers=config.Sina_Image_Header)
-        # print(r.status_code)
-        # 生成uuid
-        uuidCode = uuid.uuid1()
-        # 写入本地
-        path = f'{config.images_root_path}/{uuidCode}.jpg'
-        with open(path, 'wb') as f:
-            f.write(r.content)
-        if os.path.exists(path):
-            return [os.path.getsize(path), str(uuidCode) + '.jpg']
+        print(r.status_code)
+        if r.status_code == 200:
+            # 写入本地
+            root_path = os.path.join(os.getcwd(), config.media_path, str(mid), 'Social_Media_Data')
+            if not os.path.exists(root_path):
+                os.makedirs(root_path)
+            path = os.path.join(root_path, f'{pid}.jpg')
+            with open(path, 'wb') as f:
+                f.write(r.content)
+            if os.path.exists(path):
+                if os.path.getsize(path) != 226:
+                    return str(path)
+            else:
+                return None
         else:
             return None
     except Exception as e:
@@ -41,7 +46,7 @@ def DownloadWeiboImage(pid):
     return None
 
 
-def DownloadWeiboVideo(url):
+def DownloadWeiboVideo(mid, url):
     """
     下载微博视频
     :param url: 视频路径
@@ -49,12 +54,16 @@ def DownloadWeiboVideo(url):
     """
     try:
         uuidCode = uuid.uuid1()
-        path = f'{config.video_root_path}/{uuidCode}.mp4'
+        root_path = os.path.join(os.getcwd(), config.media_path, str(mid), 'Social_Media_Data')
+        if not os.path.exists(root_path):
+            os.makedirs(root_path)
+        path = os.path.join(root_path, f'{uuidCode}.mp4')
         urllib.request.urlretrieve(url, path)
         if os.path.exists(path):
-            return [os.path.getsize(path), str(uuidCode) + '.mp4']
+            return str(path)
         else:
             return None
     except Exception as e:
         logutils.error(e)
     return None
+
